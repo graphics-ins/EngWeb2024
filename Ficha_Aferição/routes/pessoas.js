45522ccc33c9cc80ccadb5080134922578744281
Inês Castro <73346645+graphics-ins@users.noreply.github.com>
2024-04-10 00:05:20 +0100

    Create pessoas.js

---
+++ Ficha_Aferição/routes/pessoas.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+var Pessoa = require("../controllers/pessoas")
+
+var d = new Date().toISOString().substring(0, 16)
+
+router.get('/', function(req, res, next) {
+  Pessoa.list()
+    .then(response => {
+       res.jsonp(response)
+    })
+    .catch(function(erro){
+        res.status(501).render("error", {"error": erro})
+    })
+});
+
+router.get('/:id', function(req, res, next) {
+  Pessoa.findById(req.params.id)
+    .then(response => {
+       res.jsonp(response)
+    })
+    .catch(function(erro){
+        res.status(501).render("error", {"error": erro})
+    })
+});
+
+router.post('/', function(req, res, next) {
+  result = req.body
+  Pessoa.insert(result)
+    .then(response => {
+       res.jsonp(response)
+    })
+    .catch(function(erro){
+        res.status(501).render("error", {"error": erro})
+    })
+});
+
+
+router.put('/:id', function(req, res, next) {
+  Pessoa.findById(req.params.idCompositor)
+    .then(response => {
+       res.jsonp(response)
+    })
+    .catch(function(erro){
+        res.status(501).render("error", {"error": erro})
+    })
+});
+
+
+router.delete('/:id', function(req, res, next) {
+  Pessoa.remove(req.params.idCompositor)
+    .then(response => {
+       res.jsonp(response)
+    })
+    .catch(function(erro){
+        res.status(501).render("error", {"error": erro})
+    })
+});
+
+
+module.exports = router;
